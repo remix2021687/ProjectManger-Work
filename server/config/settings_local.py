fb2db7f0c091ddc139b5fc9d2a9ea493f5ae166c
Maksym Yeromin <56311335+remix2021687@users.noreply.github.com>
2026-01-02 20:59:31 +0100

    add client and server folder and installed django to serve folder

---
+++ server/config/settings_local.py
@@ -0,0 +1,18 @@
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'TESTDB',
+        'USER': 'ROOT',
+        'PASSWORD': '2010665KE',
+        'HOST': '172.18.0.2',
+        'PORT': '5432',
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
---
+++ server/config/settings_prod.py
@@ -0,0 +1,20 @@
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', 'localhost')]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': f'{os.environ.get('DB_NAME')}',
+        'USER': f'{os.environ.get('DB_USER')}',
+        'PASSWORD': f'{os.environ.get('DB_PASSWORD')}',
+        'HOST': f"{os.environ.get('DB_HOST')}",
+        'PORT': f"{os.environ.get('DB_PORT', '5432')}",
+    }
+}
+
+CORS_ALLOWED_ORIGINS = [
+    f"{os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:5173')}"
+]
